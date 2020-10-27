5c897aa15feb97917c6b4bd9d900106475a26f1e
mjhwang80 <hmjkor@gmail.com>
2020-10-28 01:08:31 +0900

    ui 공부

--- ui/pushbuttonTest.py
+++ ui/pushbuttonTest.py
@@ -2,7 +2,7 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 
-form_class = uic.loadUiType("ui/pushbuttonTest.ui")[0]
+form_class = uic.loadUiType("ui/main.ui")[0]
 
 class WindowClass(QMainWindow, form_class):
     def __init__(self) :
